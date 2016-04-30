a221c7856f09473c9addc9af369b00030c0e2cbe
Subhankar Mukherjee <skrmuk2700c@gmail.com>
2016-04-30 15:47:30 +0530

    ADDED: Navbar Initial settings

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :blog_articles
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'visitors#index'
+  root to: 'blog_articles#index'
   devise_for :users
   get 'page/about'
   get 'page/contact'
