10111d9d377cc674d6a3bfb8e1b2dfd331d9d974
Subhankar Mukherjee <skrmuk2700c@gmail.com>
2016-05-03 12:56:56 +0530

    ADDED: Association

--- app/controllers/blog_articles_controller.rb
+++ app/controllers/blog_articles_controller.rb
@@ -1,6 +1,26 @@
 class BlogArticlesController < InheritedResources::Base
+  
+
+  def create
+    @blog_article = BlogArticle.new(blog_article_params)
+    @blog_article.user = current_user
+
+    respond_to do |format|
+      if @blog_article.save
+        format.html { redirect_to @blog_article, notice: 'Blog article was successfully created.' }
+        format.json { render :show, status: :created, location: @blog_article }
+      else
+        format.html { render :new }
+        format.json { render json: @blog_article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   private
+
+  	# def set_blog_article
+  	#   @blog_article = BlogArticle.find(params[:id])
+  	# end
 
     def blog_article_params
       params.require(:blog_article).permit(:title, :description)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  has_many :blog_articles
+
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
@@ -10,4 +13,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
 end
