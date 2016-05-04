b30bdb1db4bcfdfedfd4d36700723f5eff9b9009
Subhankar Mukherjee <skrmuk2700c@gmail.com>
2016-05-04 10:52:22 +0530

    ADDED: Data Validation

--- app/models/blog_article.rb
+++ app/models/blog_article.rb
@@ -2,4 +2,8 @@ class BlogArticle < ActiveRecord::Base
 
 	belongs_to :user
 
+	validates :title, presence: true, length: { maximum: 250 }
+
+	validates :description, presence: true, length: { minimum: 10 }
+
 end
