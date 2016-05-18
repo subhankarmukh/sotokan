baf1fca43d85ac63b2f7d6b6be0d777b1c7d1fdf
Subhankar Mukherjee <skrmuk2700c@gmail.com>
2016-05-18 13:53:08 +0530

    Bug: Problem adding name field to Comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517141933) do
+ActiveRecord::Schema.define(version: 20160518072008) do
 
   create_table "abilities", force: :cascade do |t|
   end
