59f3bd7f8445b5ddeb0adb4ccb57ddaa90e43bc8
Subhankar Mukherjee <skrmuk2700c@gmail.com>
2016-06-06 16:41:46 +0530

    ADDED: Excerpt added

--- app/helpers/blog_articles_helper.rb
+++ app/helpers/blog_articles_helper.rb
@@ -1,2 +1,5 @@
 module BlogArticlesHelper
+	def get_blog_article_excerpt(entity, length = 250)
+		truncate(CGI.unescapeHTML(entity.description).gsub(%r{</?[^>]+?>}, ''), :length => length, :escape => false, :omission => "... ")
+	end
 end
